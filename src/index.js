1cf39cf62a2d0cf0fe357af16867bdef330a9048
sunadoi <y.sunadoi@gmail.com>
2020-02-17 17:16:20 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,42 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import './index.css'
 
-const MoneyBook = () => {
-  const books = [
-    {date: "1/1", item: "お年玉", amount: 10000},
-    {date: "1/3", item: "ケーキ", amount: -500},
-    {date: "2/1", item: "小遣い", amount: 3000},
-    {date: "2/5", item: "漫画", amount: -600}
-  ]
-  return (
-    <div>
-      <Title>小遣い帳</Title>
-      <table className="book">
-        <thead>
-          <tr><th>日付</th><th>項目</th><th>入金</th><th>出金</th></tr>
-        </thead>
-        <tbody>
-          {books.map((book) => 
-            <MoneyBookItem book={book} key={book.date + book.item} />
-          )}
-        </tbody>
-      </table>
-    </div>
-  )
+class MoneyBook extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {books:[]}
+  }
+  componentDidMount() {
+    this.setState({books: [
+      {date: "1/1", item: "お年玉", amount: 10000},
+      {date: "1/3", item: "ケーキ", amount: -500},
+      {date: "2/1", item: "小遣い", amount: 3000},
+      {date: "2/5", item: "漫画", amount: -600}
+    ]})
+  }
+  addBook(date, item, amount) {
+    const book = {date: date, item: item, amount: amount}
+    this.setState({books: this.state.books.concat(book)})
+  }
+
+  render () {
+    return (
+      <div>
+        <Title>小遣い帳</Title>
+        <MoneyBookList books={this.state.books} />
+        <MoneyEntry add={(date, item, amount) => this.addBook(date, item, amount)} />
+      </div>
+    )
+  }
+}
+
+class MoneyEntry extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {date: '', item: '', amount: '', payingIn: true}
+  }
 }
 
 const MoneyBookItem = (props) => {
