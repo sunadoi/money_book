746c19001e6ef3ecdbc7d75c9359d78b279228e1
sunadoi <y.sunadoi@gmail.com>
2020-02-17 17:47:01 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -37,6 +37,63 @@ class MoneyEntry extends Component {
     super(props)
     this.state = {date: '', item: '', amount: '', payingIn: true}
   }
+  onChangeDate(event) {
+    this.setState({date: event.target.value})
+  }
+  onChangeItem(event) {
+    this.setState({item: event.target.value})
+  }
+  onChangeAmount(event) {
+    this.setState({amount: event.target.value})
+  }
+  onChanePayingIn(event) {
+    this.setState({payingIn: event.target.value == "on"})
+  }
+  onClickSubmit() {
+    this.props.add(this.state.date, this.state.item, this.state.amount * (this.state.payingIn ? 1 : -1))
+    this.setState({date: '', item: '', amount: '', payingIn: false})
+  }
+  render() {
+    return (
+      <div className="entry">
+        <fieldset>
+          <legend>記帳</legend>
+          <div>
+            <input type="radio" value="on" checked={this.state.payingIn} onChange={(event) => this.onChanePayingIn(event)} /> 入金
+            <input type="radio" value="off" checked={!this.state.payingIn} onChange={(event) => this.onChanePayingIn(event)} /> 出金
+          </div>
+          <div>日付: <input type="text" value={this.state.date} onChange={(event) => this.onChangeDate(event)} placeholder="3/15" /> </div>
+          <div>項目: <input type="text" value={this.state.item} onChange={(event) => this.onChangeItem(event)} placeholder="おこづかい" /> </div>
+          <div>金額: <input type="text" value={this.state.amount} onChange={(event) => this.onChangeAmount(event)} placeholder="1000" /> </div>
+          <div> <input type="submit" value="追加" onClick={() => this.onClickSubmit()} /> </div>
+        </fieldset>
+      </div>
+    )
+  }
+}
+
+MoneyEntry.propTypes = {
+  add: PropTypes.func.isRequired
+}
+
+const MoneyBookList = (props) => {
+  return (
+    <div>
+      <table className="book">
+        <thead  data-type="ok">
+          <tr><th>日付</th><th>項目</th><th>入金</th><th>出金</th></tr>
+        </thead>
+        <tbody>
+          {props.books.map((book) =>
+            <MoneyBookItem book={book} key={book.date + book.item} /> )}
+         </tbody>
+      </table>
+    </div>
+  )
+}
+
+MoneyBookList.propTypes = {
+  books: PropTypes.array.isRequired
 }
 
 const MoneyBookItem = (props) => {
